1fb5a072032f90beddf1aabc2a9175bbaf2ccd88
nguyenjessev <nguyenjessev@gmail.com>
2023-05-11 16:05:35 -0400

    Read uploaded file to array

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,23 @@
 <script>
-	const handleFileUpload = () => {};
+	let files;
+	let lines;
+
+	const readFile = (file) => {
+		const reader = new FileReader();
+
+		reader.onload = (e) => {
+			lines = e.target.result.split('\n');
+
+			console.log(lines);
+		};
+		reader.readAsText(file);
+	};
+
+	const handleFileUpload = () => {
+		const uploadedFile = files[0];
+
+		readFile(uploadedFile);
+	};
 </script>
 
 <h1>CSV Winner Picker</h1>
@@ -10,5 +28,8 @@
 	id="file-upload"
 	name="file-upload"
 	accept="text/csv"
+	bind:files
 	on:change={handleFileUpload}
 />
+
+<div class="file-info" />
