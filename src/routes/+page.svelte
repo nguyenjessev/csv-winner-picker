52c8aeceb6eeb15c1a0f4858535cb4fe140f8013
nguyenjessev <nguyenjessev@gmail.com>
2023-05-11 15:30:17 -0400

    Add handleFileUpload handler

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
+<script>
+	const handleFileUpload = () => {};
+</script>
+
 <h1>CSV Winner Picker</h1>
 
 <label for="file-upload">Upload a file:</label>
-<input type="file" id="file-upload" name="file-upload" accept="text/csv">
+<input
+	type="file"
+	id="file-upload"
+	name="file-upload"
+	accept="text/csv"
+	on:change={handleFileUpload}
+/>
